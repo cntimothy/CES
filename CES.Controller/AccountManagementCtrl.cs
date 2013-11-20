9c7117c37dd6d25594b2e3539cf848609c98d548
cntimothy <cntimothy1990@gmail.com>
2013-11-20 20:24:44 +0800

    ENH:finish work of change passwd

---
+++ CES.Controller/AccountManagementCtrl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CES.Controller
+{
+    public class AccountManagementCtrl
+    {
+        /// <summary>
+        /// 修改密码
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="newPassword"></param>
+        /// <param name="oldPassword"></param>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public static bool ChangePassword(string id, string newPassword, string oldPassword, ref string exception)
+        {
+            return true;
+        }
+    }
+}
--- CES.UI/Pages/AccountManagement/ChangePasswd.aspx.cs
+++ CES.UI/Pages/AccountManagement/ChangePasswd.aspx.cs
@@ -4,14 +4,55 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using CES.Controller;
 
 namespace CES.UI.Pages.AccountManagement
 {
-    public partial class ChangePasswd : System.Web.UI.Page
+    public partial class ChangePasswd : PageBase
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                //将UserID写入ViewState
+                WriteUserIDToViewState();
+            }
+        }
 
+        protected void Button_ChangePassword_Click(object sender, EventArgs e)
+        {
+            string exception = "";
+            string oldPassword = TextBox1.Text.Trim();
+            string newPassword = TextBox2.Text.Trim();
+            if (oldPassword.Length != 6 || newPassword.Length != 6)
+            {
+                showError("修改失败！", "密码必须六位！");
+                return;
+            }
+            if (checkNull(oldPassword, newPassword))
+            {
+                string id = Session["UserID"].ToString();
+                if (AccountManagementCtrl.ChangePassword(id, newPassword, oldPassword, ref exception))
+                {
+                    showInformation("修改成功！");
+                }
+                else
+                {
+                    showError("修改失败！", exception);
+                }
+            }
+        }
+
+        private bool checkNull(params string[] items)
+        {
+            foreach (string item in items)
+            {
+                if (item == "")
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
