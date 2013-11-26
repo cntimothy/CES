9662c9d8e7f488dbfa7f2489417d8b915cea1ad4
cntimothy <cntimothy1990@gmail.com>
2013-11-26 21:09:36 +0800

    ENH:change evaluation stage

--- CES.Controller/CommonCtrl.cs
+++ CES.Controller/CommonCtrl.cs
@@ -16,7 +16,7 @@ namespace CES.Controller
         /// <returns></returns>
         public static bool GetCurrentStage(ref EvaluationStage evaluationStage, ref string exception)
         {
-            evaluationStage = EvaluationStage.STARTED;
+            evaluationStage = EvaluationStage.UNSTARTED;
             return true;
         }
 
