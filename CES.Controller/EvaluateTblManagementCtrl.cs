d16e8917fe679c51321349930d792323b8e960a2
cntimothy <cntimothy1990@gmail.com>
2013-12-05 09:37:24 +0800

    ENH:add docs

---
+++ CES.Controller/EvaluateTblManagementCtrl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CES.DataStructure;
+
+namespace CES.Controller
+{
+    public class EvaluateTblManagementCtrl
+    {
+        /// <summary>
+        /// 更新指定职务的考核表，如果没有则插入。成功返回true，否则返回false
+        /// </summary>
+        /// <param name="jobID">指定的职务</param>
+        /// <param name="evaluateTbl">考核表</param>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public static bool UpdateEvaluateTbl(string jobID, EvaluateTbl evaluateTbl, ref string exception)
+        {
+            return true;
+        }
+    }
+}
